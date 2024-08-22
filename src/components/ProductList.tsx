af502b1ad696225f106f7d7d8b35849c3e89c792
Evian <allainevan1@gmail.com>
2024-08-22 13:22:22 +0200

    Modifs img

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,7 +6,7 @@ const ProductList = () => {
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
             <div className="relative w-full h-80">
-            <Image src="https://images.pexels.com/photos/17697811/pexels-photo-17697811/free-photo-of-mer-eau-bateau-bateau-a-moteur.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            <Image src="https://images.pexels.com/photos/4465121/pexels-photo-4465121.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt=""
             fill
             sizes="25vw"
@@ -28,7 +28,7 @@ const ProductList = () => {
             </Link>
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
             <div className="relative w-full h-80">
-            <Image src="https://images.pexels.com/photos/17697811/pexels-photo-17697811/free-photo-of-mer-eau-bateau-bateau-a-moteur.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            <Image src="https://images.pexels.com/photos/6621282/pexels-photo-6621282.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt=""
             fill
             sizes="25vw"
@@ -50,7 +50,7 @@ const ProductList = () => {
             </Link>
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
             <div className="relative w-full h-80">
-            <Image src="https://images.pexels.com/photos/17697811/pexels-photo-17697811/free-photo-of-mer-eau-bateau-bateau-a-moteur.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            <Image src="https://images.pexels.com/photos/6467798/pexels-photo-6467798.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt=""
             fill
             sizes="25vw"
@@ -72,7 +72,7 @@ const ProductList = () => {
             </Link>
             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
             <div className="relative w-full h-80">
-            <Image src="https://images.pexels.com/photos/17697811/pexels-photo-17697811/free-photo-of-mer-eau-bateau-bateau-a-moteur.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            <Image src="https://images.pexels.com/photos/4210370/pexels-photo-4210370.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt=""
             fill
             sizes="25vw"
